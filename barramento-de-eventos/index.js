09d178cf79b95ebd5006c9245deb9c9dacb44696
diogo163 <diogo.pavan163@gmail.com>
2024-11-08 15:10:08 -0300

    feat(t2): implementa mss de logs

--- barramento-de-eventos/index.js
+++ barramento-de-eventos/index.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-
 const axios = require('axios');
 
 const app = express();
@@ -11,12 +10,11 @@ const eventos = []
 app.post('/eventos', (req, res) => {
     const evento = req.body;
     eventos.push(evento)
-
     axios.post('http://localhost:4000/eventos', evento);
     axios.post('http://localhost:5000/eventos', evento);
     axios.post("http://localhost:6000/eventos", evento);
     axios.post("http://localhost:7000/eventos", evento);
-
+    axios.post("http://localhost:8000/eventos", evento);
 
     res.status(200).send({ msg: "ok" });
 });
