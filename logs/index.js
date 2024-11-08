98630742677d8f4321f8466e2441d8885ac79687
diogo163 <diogo.pavan163@gmail.com>
2024-11-08 15:42:17 -0300

    feat(t2): implanta com Docker

--- logs/index.js
+++ logs/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const axios = require('axios');
 const { v4: uuidv4 } = require('uuid');
-
 const app = express();
 app.use(express.json());
 
 const logs = [];
+
 
 const adicionarLog = (tipo) => {
   const dataHora = new Date().toLocaleString('pt-BR', {
@@ -22,18 +22,26 @@ const adicionarLog = (tipo) => {
     tipo_evento: tipo,
     data_hora: dataHora
   });
+  console.log(`Log registrado: ${tipo} em ${dataHora}`);
 };
-//
+
+
 app.post('/eventos', (req, res) => {
   const { tipo } = req.body;
+  if (!tipo) {
+    return res.status(400).send({ msg: 'Tipo de evento é obrigatório!' });
+  }
+
   adicionarLog(tipo);
   res.status(200).send({ msg: 'Log registrado!' });
 });
+
 
 app.get('/logs', (req, res) => {
   res.status(200).send(logs);
 });
 
+
 app.listen(8000, () => {
-  console.log('Log. Porta 8000');
+  console.log('Microsserviço de logs ativo. Porta 8000');
 });
