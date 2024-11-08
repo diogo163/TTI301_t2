37779968474961471f8c339aa950e6f6f2aa8a97
diogo163 <diogo.pavan163@gmail.com>
2024-11-08 14:56:47 -0300

    feat(t2): implementa solucao vista em aula

---
+++ barramento-de-eventos/index.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const axios = require('axios');
+
+const app = express();
+app.use(bodyParser.json());
+
+const eventos = []
+
+app.post('/eventos', (req, res) => {
+    const evento = req.body;
+    eventos.push(evento)
+
+    axios.post('http://localhost:4000/eventos', evento);
+    axios.post('http://localhost:5000/eventos', evento);
+    axios.post("http://localhost:6000/eventos", evento);
+    axios.post("http://localhost:7000/eventos", evento);
+
+
+    res.status(200).send({ msg: "ok" });
+});
+
+app.listen(10000, () => {
+console.log('Barramento de eventos. Porta 10000.')
+})
+
+app.get('/eventos', (req, res) => {
+    res.send(eventos)
+})
